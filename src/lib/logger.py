0a6db4f00b62b7127eb3dd907f00252f3733e2f3
dicksimon <>
2024-02-15 13:28:19 +0100

    improve logging for pages

--- src/lib/logger.py
+++ src/lib/logger.py
@@ -1,27 +1,41 @@
 
 from enum import Enum
+from collections import defaultdict
 
-log_page = Enum('log_pages',['CREATED_SWAP','CREATED_RAM','MOVED_SWAP', 'MOVED_RAM', 'FREED'])
+log_page = Enum('log_pages',['CREATED_SWAP','CREATED_RAM','MOVED_SWAP', 'MOVED_RAM', 'PAGE_USED', 'PAGE_UNUSED','FREED'])
 
 
 class Logger:
     
     processes: dict[int,list] = {}
-    pages: dict[tuple[int,int],list] = {}
+    pages =  defaultdict(dict[int,list])
 
 
     def create_page(self, key, ram):
        if ram:
-           self.pages.update({key:[log_page.CREATED_RAM]})
+           self.pages[key[0]][key[1]] = [log_page.CREATED_RAM]
        else:
-            self.pages.update({key:[log_page.CREATED_SWAP]})
+           self.pages[key[0]][key[1]] = [log_page.CREATED_SWAP]
 
     def move_page(self, key, ram):
         if ram:
-            self.pages[key].append(log_page.MOVED_RAM)
+            self.pages[key[0]][key[1]].append(log_page.MOVED_RAM)
         else:
-            self.pages[key].append(log_page.MOVED_SWAP)
-        pass
+            if self.pages[key[0]][key[1]][-1] == log_page.PAGE_USED:
+                pass
+            self.pages[key[0]][key[1]].append(log_page.MOVED_SWAP)
+        
 
     def free_page(self,key):
-        self.pages[key].append(log_page.FREED)
+        self.pages[key[0]][key[1]].append(log_page.FREED)
+
+    def page_use(self,key, use):
+        if use:
+            if len(self.pages[key[0]][key[1]]) > 3:
+                testlist = self.pages[key[0]][key[1]][-6:]
+                if  testlist == [log_page.MOVED_SWAP, log_page.PAGE_UNUSED, log_page.PAGE_USED, log_page.MOVED_RAM, log_page.MOVED_SWAP, log_page.PAGE_UNUSED]:
+                    pass
+            self.pages[key[0]][key[1]].append(log_page.PAGE_USED)
+            
+        else:
+            self.pages[key[0]][key[1]].append(log_page.PAGE_UNUSED)
