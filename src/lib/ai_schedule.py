ef06fa90b851322baef7705bffa232b7a4b297f8
dicksimon <>
2024-05-10 10:38:02 +0200

    update ai schedule

--- src/lib/ai_schedule.py
+++ src/lib/ai_schedule.py
@@ -13,13 +13,14 @@ class AiSchedule(scheduler_base.SchedulerBase):
 
 
     def schedule(self):
+        cpu_owner = {}
         #clean io_queue
         self.clean_io_queue()
         #update ram schedule 
-        self.update_ram_schedule(self.calculate_ram_from_cpu_schedule(self.cpu_owner))
+        self.update_ram_schedule(self.calculate_ram_from_cpu_schedule(cpu_owner))
         #update cpu schedule
-        self.update_cpu_schedule(self.cpu_owner)
-
-    def get_events(self):
+        self.update_cpu_schedule(cpu_owner)
         return self._event_queue
 
+    def clear_sched_events(self):
+        self._event_queue.clear()
