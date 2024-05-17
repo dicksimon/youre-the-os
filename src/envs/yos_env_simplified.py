e2e1071592405bb6c32bf44920e9680738d4cb59
dicksimon <>
2024-05-17 10:47:30 +0200

    env: implement obervation mapping

--- src/envs/yos_env_simplified.py
+++ src/envs/yos_env_simplified.py
@@ -1,6 +1,7 @@
 import gymnasium as gym
 from gymnasium import spaces
 from gymnasium.spaces import  Dict,MultiDiscrete, MultiBinary
+
 import pygame
 import numpy as np
 import asyncio
@@ -8,9 +9,11 @@ from os import path
 import sys
 import argparse
 from enum import Enum
+from typing import List
 
 from lib import ai_schedule
 from lib import event_manager
+from lib.constants import _TIME_TO_UNSTARVE_MS
 from scenes.game import Game
 from scene_manager import scene_manager
 from game_info import TITLE
@@ -21,23 +24,11 @@ FPS = 60
 
 def compile_auto_script():
     return None
-    if len(sys.argv) == 1:
-        return None
-    try:
-        source_file = sys.argv[1]
-        if not path.isabs(source_file):
-            source_file = '../' + source_file
-        print('reading source file' , source_file, file=sys.stderr)
-        with open(source_file, encoding="utf_8") as in_file:
-            source = in_file.read()
-        return compile(source, source_file, 'exec')
-    except (SyntaxError, ValueError):
-        print('Compilation failed, ignoring argument', file=sys.stderr)
-        return None
+
 
 
 class YosEnvSimplified(gym.Env):
-    metadata = {"render_modes": ["human"], "render_fps": FPS}
+    metadata = {"render_modes": ["human", "None"], "render_fps": FPS}
 
     def __init__(self, render_mode="human"):
 
@@ -47,12 +38,16 @@ class YosEnvSimplified(gym.Env):
 
         self.action_space = spaces.MultiDiscrete([57 for _ in range(16)])
 
+        self.observation_space = Dict({
+            "exists": MultiBinary(57,),
+            "unstarve_time": MultiDiscrete([5 for _ in range(57)]),
+            "starvation_level": MultiDiscrete([6 for _ in range(57)]),
+            "has_cpu": MultiBinary(57,),
+            "waiting_for_page": MultiBinary(57,)
+        })
 
-        #self.action_space = spaces.Discrete(4)
 
-
-        self.observation_space = spaces.Discrete(4)
-
+        self.observation_mapping: List[Dict[str, np.ndarray]] = []
 
 
     def reset(self, seed=None, options=None):
@@ -62,7 +57,6 @@ class YosEnvSimplified(gym.Env):
 
         # We need the following line to seed self.np_random
         super().reset(seed=seed)
-        observation = self.get_obs()
 
         difficulty_config = {'name': 'Insane',
                             'num_cpus': 16, 'num_processes_at_startup': 42,
@@ -78,7 +72,7 @@ class YosEnvSimplified(gym.Env):
             self.screen = None
         self.clock = pygame.time.Clock()
         self.scenes = {}
-        self.game_scene = Game(self.screen, self.scenes, difficulty_config, compile_auto_script(), standalone=True, ai_mode=True)
+        self.game_scene = Game(self.screen, self.scenes, difficulty_config, None, standalone=True, ai_mode=True)
         self.scenes['game'] = self.game_scene
         self.game_scene.start()
 
@@ -86,11 +80,11 @@ class YosEnvSimplified(gym.Env):
         self.scene_manager = scene_manager
         self.event_manager.clear_events()
         self.scheduler = ai_schedule.AiSchedule()
+        observation = self.get_obs()
 
         if self.render_mode == "human":
 
 
-            
             icon = pygame.image.load(path.join('assets', 'icon.png'))
             pygame.display.set_caption(TITLE)
             pygame.display.set_icon(icon)
@@ -116,7 +110,46 @@ class YosEnvSimplified(gym.Env):
 
     
     def get_obs(self):
-        return self.observation_space.sample()
+        exists = np.ones([57], dtype=np.int8)
+        unstarve_time = np.zeros([57], dtype=np.int8)
+        starvation_level = np.zeros([57], dtype=np.int8)
+        has_cpu = np.zeros([57],dtype=np.int8)
+        waiting_for_page = np.zeros([57], dtype=np.int8)
+             
+        for pid in range(57):
+            self.scheduler
+            if pid not in self.scheduler.processes:
+                exists[pid] = 0
+
+            else:
+                if pid in self.scheduler.cpus_active:
+                    has_cpu[pid] = 1
+
+                if self.scheduler.processes[pid].waiting_for_page:
+                    waiting_for_page[pid] = 1
+
+
+            for job_time in self.scheduler.job_times.keys():
+                if pid in self.scheduler.job_times[job_time]:
+                    unstarve_time[pid] = (job_time/(0.5 * _TIME_TO_UNSTARVE_MS)) - 1 
+                    break
+
+            for level in self.scheduler.starvation.keys():
+                if [process for process in self.scheduler.starvation[level] if process[0] == pid]:
+                    starvation_level[pid] = level
+                    break
+
+
+        observation = {
+            "exists": exists,
+            "unstarve_time": unstarve_time,
+            "starvation_level": starvation_level,
+            "has_cpu": has_cpu,
+            "waiting_for_page": waiting_for_page
+        }
+
+
+        return observation
 
 
     async def game_step(self, action):
