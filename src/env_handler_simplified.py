393c66c34591c989928ff6e81e9083708c9426c0
dicksimon <>
2024-05-23 12:28:31 +0200

    env handler: add arguments, implement load handling

--- src/env_handler_simplified.py
+++ src/env_handler_simplified.py
@@ -1,10 +1,15 @@
+import argparse
+import os
+
 import gymnasium  as gym
 from gymnasium.experimental.wrappers import FlattenObservationV0
 from gymnasium.envs.registration import register
 from stable_baselines3 import A2C
 from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.vec_env import SubprocVecEnv, VecNormalize, DummyVecEnv
+from stable_baselines3.common.monitor import Monitor
 
-import argparse
+
 
 class RangedInt:
     """Type validator for argparse"""
@@ -34,39 +39,46 @@ def parse_arguments():
     parser.add_argument('--steps',
         type=RangedInt('steps', 1, 100000),
         help="number of total timesteps (1-100000)")
+
+    parser.add_argument('--render',action='store', help="render mode", required=False)
+    parser.add_argument('--filestore',action='store', help="filename to store model", required=True)
+    parser.add_argument('--fileload',action='store', help="filename to load model", required=False)
     args = parser.parse_args()
 
-    return args.steps
+    return args.steps, args.render, args.filestore,  args.fileload
+
+if __name__=="__main__":
+
+    init = True
+    timesteps,  render, filestore, fileload = parse_arguments()
+
+    log_dir = "/tmp/gym/" + filestore
+    os.makedirs(log_dir, exist_ok=True)
+
+    gym.register(
+        id='YOS',
+        entry_point='envs.yos_env_simplified:YosEnvSimplified',
+        kwargs={'render_mode': 'None' 'human'}
+    )
+
+    if not fileload:
+        env = gym.make("YOS")
+        env = Monitor(env, log_dir)
+        model = A2C("MultiInputPolicy", env, device="cpu")
+
+    else:
+        if render:
+            env=gym.make("YOS", render_mode= 'human')
+        else:
+            env=gym.make("YOS")
+        env = Monitor(env,log_dir)
+        model = A2C.load("fileload", env)
+
+    model.learn(total_timesteps=timesteps)
+    model.save("filestore")
+    del model
+ 
 
 
 
-
-# Register the environment
-gym.register(
-    id='YOS-v0',
-    entry_point='envs.yos_env_simplified:YosEnvSimplified',
-    kwargs={'render_mode': 'None'}
-)
-
-timesteps = parse_arguments()
-
-env = gym.make("YOS-v0")
-print (env.observation_space.shape)
-env = FlattenObservationV0(env)
-print (env.observation_space.shape)
-
-
-model = A2C("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=timesteps)
-model.save("a2c_yos_env")
-del model 
-model = A2C.load("a2c_yos_env")
-obs = env.reset()
-
-
-while True:
-    action, _states = model.predict(obs)
-    obs, rewards, dones, info = env.step(action)
-    print(rewards)
-    env.render("human")
 
